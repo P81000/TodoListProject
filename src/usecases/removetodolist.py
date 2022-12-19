c36378b4cd56be51b84970e6ec63c690caf14e91
P81000 <p.masteguin@unifesp.br>
2022-12-19 17:50:56 -0300

    Test remove todo list - Handle invalid user

--- src/usecases/removetodolist.py
+++ src/usecases/removetodolist.py
@@ -1,8 +1,12 @@
-
+from src.usecases.errors.invalidusererror import InvalidUserError
 class RemoveTodoList:
-    def __init__(self, todolist_repo):
+    def __init__(self, todolist_repo, user_repo):
         self.todolist_repo = todolist_repo
+        self.user_repo = user_repo
 
     def perform(self, user_email):
         todolist = self.todolist_repo.find_by_email(user_email)
+        user = self.user_repo.find_by_email(user_email)
+        if not user:
+            raise InvalidUserError()
         self.todolist_repo.delete(user_email)
--- test/usecases/removetodolist_test.py
+++ test/usecases/removetodolist_test.py
@@ -5,8 +5,10 @@ from src.usecases.createtodolist import CreateTodoList
 from src.usecases.createtodoitem import CreateTodoItem
 from src.usecases.signup import SignUp
 from src.usecases.removetodolist import RemoveTodoList
+from src.usecases.errors.invalidusererror import InvalidUserError
 from src.usecases.createtodolist import TodoList
 from src.usecases.createtodoitem import TodoItem
+import pytest
 
 def test_remove_todo_list():
     user_repo = InMemoryUserRepository()
@@ -20,7 +22,15 @@ def test_remove_todo_list():
     SignUp(user_repo, hash_service).perform(user_name, user_email, user_password)
     CreateTodoList(user_repo, todolist_repo).perform(user_email)
     CreateTodoItem(todolist_repo).perform(user_email, item_description, item_priority)
-    usecase = RemoveTodoList(todolist_repo)
+    usecase = RemoveTodoList(todolist_repo, user_repo)
     usecase.perform(user_email)
     persisted_todo_list = todolist_repo.find_by_email(user_email)
     assert persisted_todo_list is None
+
+def test_remove_todo_list_with_invalid_user():
+    user_repo = InMemoryUserRepository()
+    todolist_repo = InMemoryTodoListRepository()
+    user_email = 'invalid@email.com'
+    usecase = RemoveTodoList(todolist_repo, user_repo)
+    with pytest.raises(InvalidUserError):
+        usecase.perform(user_email)
